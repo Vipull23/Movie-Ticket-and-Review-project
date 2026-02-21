cfd49c36c84f23ed6db6a86971ffbd3516a27f6c
Vipull23 <vipulsharma23.vs@gmail.com>
2026-02-22 00:24:53 +0530

    22-02-2026

---
+++ Movie-Ticket-and-Reviewing-System/src/main/java/org/movieproject/Movie/Ticket/and/Reviewing/System/MovieTicketAndReviewingSystemApplication.java
@@ -0,0 +1,13 @@
+package org.movieproject.Movie.Ticket.and.Reviewing.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketAndReviewingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketAndReviewingSystemApplication.class, args);
+	}
+
+}
