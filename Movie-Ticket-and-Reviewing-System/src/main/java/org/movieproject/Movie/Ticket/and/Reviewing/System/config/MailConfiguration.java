6c0eb3b343d75507155e46d52284556892cada7f
Vipull23 <vipulsharma23.vs@gmail.com>
2026-03-05 15:51:09 +0530

    5-03-2026

---
+++ Movie-Ticket-and-Reviewing-System/src/main/java/org/movieproject/Movie/Ticket/and/Reviewing/System/config/MailConfiguration.java
@@ -0,0 +1,36 @@
+package org.movieproject.Movie.Ticket.and.Reviewing.System.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import tools.jackson.databind.ObjectMapper;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfiguration {
+
+    @Bean
+    public JavaMailSenderImpl getJavaMailSenderImpl() {
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("<EMAIL>");
+        mailSender.setPassword("<PASSWORD>");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+    @Bean
+    public ObjectMapper getObjectMapper() {
+        return new ObjectMapper();
+    }
+}
